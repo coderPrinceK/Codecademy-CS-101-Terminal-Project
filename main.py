99f9bb62b59227d2f2eed0958576a60f43afd4c0
Conrad Krantz <conradkrantz@gmail.com>
2023-11-09 20:48:15 -0600

    basic set up for dice added

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import random
 from Player import Player
+#Test User
 User = Player("Conrad", 1,2,3,4,5,6,7,8,9,10,11,12,13)
+
+
+#dice set up
+die_one = random.randint(1,6)
+die_two = random.randint(1,6)
+die_three = random.randint(1,6)
+die_four = random.randint(1,6)
+die_five = random.randint(1,6)
+die_six = random.randint(1,6)
+
+dice = [die_one, die_two, die_three, die_four, die_five, die_six]
+print(dice)
